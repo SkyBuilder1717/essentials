715fd07ad7651b66fe1fe514eb4a96a44b702868
SkyBuilder1717 <skybuilderoffical@yandex.ru>
2025-08-15 19:21:35 +0300

    wait

--- commands.lua
+++ commands.lua
@@ -277,8 +277,6 @@ local function vanish_cmd(name, param)
         prop = {
             visual_size = {x = 0, y = 0, z = 0},
             is_visible = false,
-            nametag = "",
-            nametag_bgcolor = {r=0,g=0,b=0,a=255}, 
             nametag_color = {r=0,g=0,b=0,a=255},
             pointable = false,
             makes_footstep_sound = false,
@@ -302,7 +300,6 @@ local function vanish_cmd(name, param)
             visual_size = {x = 1, y = 1, z = 1},
             is_visible = true,
             nametag_color = {r=255,g=255,b=255,a=0},
-            nametag = "",
             pointable = true,
             makes_footstep_sound = true,
             show_on_minimap = true,
